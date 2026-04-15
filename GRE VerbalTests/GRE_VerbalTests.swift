8ba4ea2157ef38f4173be6e19f829957b1c437a5
Martin Olivares <martin.olivarest@utem.cl>
2026-04-14 22:21:38 -0400

    test: add unit and UI test templates

---
+++ GRE VerbalTests/GRE_VerbalTests.swift
@@ -0,0 +1,17 @@
+//
+//  GRE_VerbalTests.swift
+//  GRE VerbalTests
+//
+//  Created by Martin Olivares on 23-01-26.
+//
+
+import Testing
+@testable import GRE_Verbal
+
+struct GRE_VerbalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
